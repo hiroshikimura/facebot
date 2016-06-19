658feff0924b03d90bd62e6bc1ecf916417d5366
hiroshikimura <hiroshikimura.dot.com@gmail.com>
2016-06-19 09:35:19 +0900

    トークン追加

---
+++ botrun.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd `dirname $0`
+
+while [ 1=1 ]
+do
+  #git pull
+  #bundle exec rake facebot:exec["./config/account.yml"]
+
+  if [ -e /var/tmp/facebot_status ]; then
+    echo '再起動します'
+  else
+    echo '終了します'
+    break
+  fi
+done
